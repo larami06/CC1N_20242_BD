846e8da0a5a042c6ab265a799fa6cf8a6c8edc1b
Larami <laramf@outlook.com>
2024-10-30 22:31:08 -0300

    30-10

--- PORTFOLIO/DML/EST_CASO_II_DML.sql
+++ PORTFOLIO/DML/EST_CASO_II_DML.sql
@@ -1,52 +1,29 @@
-#ALTERANDO EST_CASO_II
-CREATE DATABASE EST_CASO_II;
+# MOSTRANDO A COLUNA DE PASSAGEIROS E INSERINDO VALORES
+SHOW COLUMNS FROM passageiros;
+INSERT INTO passageiros(pass_cpf, pass_nome, pass_telefone, pass_rua, pass_numrua, pass_bairro, pass_cidade, pass_estado, pass_pais, pass_codpostal, pass_email)
+VALUES('882992023', 'Alisson', '21 99349-2384','Rio Dourado', '34', 'Santa Brisa', 'Terra Alta', 'Serranópolis', 'Brasil', '2838934112', 'alisson@gmail.com');
 
-#ALTERANDO TABELA DE PASSAGEIROS
-ALTER TABLE passageiros COMMENT = 'Tabela de informações dos passageiros';
-ALTER TABLE passageiros ADD pass_cep varchar(14) not null;
-ALTER TABLE passageiros DROP COLUMN pass_pais;
-ALTER TABLE passageiros MODIFY COLUMN pass_cidade varchar(50) not null;
-ALTER TABLE passageiros CHANGE COLUMN pass_numrua pass_numero_rua varchar(45);
+# MOSTRANDO A COLUNA DE VOOS E INSERINDO VALORES
+SHOW COLUMNS FROM voos;
+INSERT INTO voos(voo_id, voo_data, voo_hora, voo_aeronave)
+VALUES('22383', '2025-02-10', '13:30:00', 'AJF-9349');
 
-#ALTERANDO TABELA DE VOOS
-ALTER TABLE voos COMMENT = 'Tabela de informações dos voos';
-ALTER TABLE voos ADD voo_aeron_origem text;
-ALTER TABLE voos ADD voo_aeron_destino text;
-ALTER TABLE voos DROP COLUMN voo_hora;
-ALTER TABLE voos MODIFY COLUMN voo_aeronave varchar(30) not null;
-ALTER TABLE voos CHANGE COLUMN voo_hora voo_horario time not null;
+# MOSTRANDO A COLUNA DE AEROPORTOS E INSERINDO VALORES
+SHOW COLUMNS FROM aeroportos;
+INSERT INTO aeroportos(aerop_cod, aerop_nome, aerop_cidade, aerop_pais, aerop_latitude, aerop_longitude)
+VALUES('344234', 'SkyBlue', 'Norte Real', 'Brasil', '37.7749', '122.4194');
 
-#ALTERANDO TABELA DE AEROPORTOS
-ALTER TABLE voos COMMENT = 'Tabela de informações dos aeroportos';
-ALTER TABLE aeroportos ADD aerop_horarios time;
-ALTER TABLE aeroportos DROP COLUMN aerop_pais;
-ALTER TABLE aeroportos MODIFY COLUMN aerop_cidade varchar(50) not null;
-ALTER TABLE aeroportos CHANGE COLUMN aerop_latitude aerop_lat decimal(12,9) not null;
-#ALTERANDO TABELA DE AERONAVES
-ALTER TABLE voos COMMENT = 'Tabela de informações das aeronaves';
-ALTER TABLE aeronaves ADD aeron_desc text;
-ALTER TABLE aeronaves DROP COLUMN aeron_fabricante;
-ALTER TABLE aeronaves MODIFY COLUMN aeron_autonomia varchar (50) not null;
-ALTER TABLE aeronaves CHANGE COLUMN aeron_anofabr aeron_ano_de_fabr date not null;
+# MOSTRANDO A COLUNA DE AERONAVES E INSERINDO VALORES
+SHOW COLUMNS FROM aeronaves;
+INSERT INTO aeronaves(aeron_prefixo, aeron_anofabr, aeron_fabricante, aeron_capacidade, aeron_autonomia)
+VALUES('132245', '2018-03-30', 'EcoTex', '100', '16.000');
 
-#ALTERANDO TABELA DE FUNCIONARIOS
-ALTER TABLE voos COMMENT = 'Tabela de informações dos funcionarios';
-ALTER TABLE funcionarios ADD func_email varchar(30);
-ALTER TABLE funcionarios DROP COLUMN func_codpostal;
-ALTER TABLE funcionarios MODIFY COLUMN func_pais varchar(50) not null;
-ALTER TABLE funcionarios CHANGE COLUMN func_numrua func_num_da_rua int not null;
+# MOSTRANDO A COLUNA DE FUNCIONÁRIOS E INSERINDO VALORES
+SHOW COLUMNS FROM funcionarios;
+INSERT INTO funcionarios(func_id, func_nome, func_datanasc, func_rua, func_numrua, func_bairro, func_cidade, func_estado, func_pais, func_codpostal, func_cpf, func_funcao)
+VALUES('1102','Bruna','2002-12-01','Arroyo Grande', '58', 'Lagoa Branca', 'Nova Aliança','Encantado', 'Brasil', '1987240512', '32322402320', 'Comissária de Bordo');
 
-#ALTERANDO TABELA DE EQUIPES
-ALTER TABLE voos COMMENT = 'Tabela de informações das equipes';
-ALTER TABLE equipes ADD equipes_quant int not null;
-ALTER TABLE equipes DROP COLUMN funcionario_id;
-
-#ALTERANDO TABELA DE RESERVAS
-ALTER TABLE voos COMMENT = 'Tabela de informações das reservas';
-ALTER TABLE reservas ADD reservas_horario time not null;
-ALTER TABLE reservas DROP COLUMN reserva_cod;
-ALTER TABLE reservas MODIFY COLUMN passageiro_cpf varchar(12);
-ALTER TABLE reservas CHANGE COLUMN id_do_voo id_voo int;
-
-DROP SCHEMA EST_CASO_II;
-
+# MOSTRANDO A COLUNA DE RESERVAS E INSERINDO VALORES
+SHOW COLUMNS FROM reservas;
+INSERT INTO reservas(reserva_cod, passageiro_cpf, id_do_voo)
+VALUES('550', '882992023', '22383');
